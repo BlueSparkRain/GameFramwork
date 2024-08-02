6aad80d3dff3be725997ae6efa99a35a828c3f4f
XiaoXiaoXuXu <160853850+XUjun6666@users.noreply.github.com>
2024-08-02 23:22:59 +0800

    Singleton

---
+++ Assets/Scripts/CommonTools/MonoSingleton.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+/// <summary>
+/// 自动挂载式的继承Mono的单例脚本基类，推荐使用，无需手动挂载，无需考虑切场问题，动态添加，
+/// 尽量不要手动挂载
+/// </summary>
+/// <typeparam name="T"></typeparam>
+[DisallowMultipleComponent]//禁止在同一个gamobject上挂载多个相同此脚本
+public class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                //手动挂载式
+                instance = FindAnyObjectByType<T>();
+                //自动挂载式
+                if (instance == null)
+                {
+                //非挂载式单例在被调用时直接创建
+                  GameObject newManager=new GameObject(typeof(T) + "SingleManager");
+                  instance.Init(newManager);
+                  DontDestroyOnLoad(newManager);
+                }
+            }  
+            return instance;
+        }
+        private set { }
+    }
+    protected virtual void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(this);
+            return;
+        }
+
+        //手动挂载式初始化
+        if (instance == null)
+        {
+            instance = this as T;
+        }
+        DontDestroyOnLoad (this.gameObject);
+    }
+
+    public virtual void Init(GameObject newManager)
+    {
+       instance=newManager.AddComponent<T>();
+   }
+}
+
+
